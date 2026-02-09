9f996cfbfbd9bfd61d91a6098c6d16c8ec5c0c5e
Amina <minassouane@gmail.com>
2026-02-09 14:03:19 +0100

    Fix test bug by exporting message

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import { Gameboard } from "./gameboard";
 
 const board = new Gameboard();
 
-const message1 = board.placeShip(board.ships[0], false, 0, 0);
-const message2 = board.placeShip(board.ships[1], false, 0, 1);
+export const message1 = board.placeShip(board.ships[0], false, 0, 0);
+export const message2 = board.placeShip(board.ships[1], false, 0, 1);
+
+console.log(board.board);
